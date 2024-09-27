ff1bd0996fcc06045c27fb6a7aa0d18a584c872a
David Marcos <davidmarcosg98@gmail.com>
2024-09-27 22:47:10 +0200

    test

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'david',
     password: '22798',
-    host: 'localhost',
+    host: 'iwws40kwsgcscg0cwgooo4og',
     port: 5432, // default Postgres port
     database: 'ddtrack'
 });
